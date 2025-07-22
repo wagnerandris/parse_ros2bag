e89c38badfea3e992ea20d4b98ed38e92f876662
Wágner András <b31ze5@inf.elte.hu>
2025-07-22 15:21:09 +0200

    pcd and misc export

--- parse_ros2bag.py
+++ parse_ros2bag.py
@@ -5,33 +5,71 @@ import rosbag2_py
 import subprocess
 
 
-def get_topics(bag):
-
-    image_topics = []
-    return image_topics
-
-
 def parse_ros2bag(bag, output, camera_num, blur):
+    print(bag)
+
+    image_topic_names = []
+    pointcloud_topic_names = []
+    misc_topic_names = []
+
+    topic_types = {
+            'std_msgs/msg/Int32': misc_topic_names,
+
+            'sensor_msgs/msg/Image': image_topic_names,
+            'sensor_msgs/msg/PointCloud2': pointcloud_topic_names,
+            'sensor_msgs/msg/NatSatFix': misc_topic_names,
+            'sensor_msgs/msg/TimeReference': misc_topic_names,
+
+            'geometry_msgs/msg/TwistStamped': misc_topic_names,
+
+            'tf2_msgs/msg/TFMessage': misc_topic_names,
+            }
+    # TODO some of these are not just misc
+
     # get topics
     info = rosbag2_py.Info()
     metadata = info.read_metadata(bag, 'sqlite3')
 
-    # image topics
-    image_topic_names = [t.topic_metadata.name
-                        for t in metadata.topics_with_message_count
-                        if t.topic_metadata.type == 'sensor_msgs/msg/Image']
+    # separate topic types we care about into lists
+    for t in metadata.topics_with_message_count:
+        print(t.topic_metadata.name, '\t', t.topic_metadata.type, '\n')
+        topic_names = topic_types.get(t.topic_metadata.type)
+        if topic_names is not None:
+            topic_names.append(t.topic_metadata.name)
 
+    # export pointclouds
+    for t in pointcloud_topic_names:
+        subprocess.Popen([
+            'ros2', 'bag', 'export',
+            '--in', bag,
+            '-t', t, 'pcd',
+            '--dir', output + t,
+            ])
+
+    # export images
     image_export_processes = []
     for t in image_topic_names:
         image_export_processes.append(subprocess.Popen([
-            "ros2", "bag", "export",
-            "--in", bag,
-            "-t", t, "image",
-            "--dir", '.' + t
+            'ros2', 'bag', 'export',
+            '--in', bag,
+            '-t', t, 'image',
+            '--dir', output + t,
             ]))
 
+    # extract misc topics
+    misc_extract_process = subprocess.Popen([
+        'ros2', 'bag', 'extract',
+        bag,
+        '-t', *misc_topic_names,
+        '-o', output + '/misc_topics'
+        ])
+
+    # wait for image exports to finish
     for p in image_export_processes:
         p.wait()
+
+    # wait for misc extract to finish
+    misc_extract_process.wait()
 
     # TODO sync
     # TODO export synced images
@@ -45,7 +83,6 @@ def parse_ros2bag(bag, output, camera_num, blur):
     # TODO convert to kml
 
     # TODO file structure
-    
 
 
 if __name__ == '__main__':
