d39872bb7b8733591f0239c664f8ff95d6c850a0
Wágner András <b31ze5@inf.elte.hu>
2025-07-22 20:39:00 +0200

    syncing and blurring

--- parse_ros2bag.py
+++ parse_ros2bag.py
@@ -3,28 +3,26 @@
 import argparse
 import rosbag2_py
 import subprocess
+import os
+
+scriptpath = os.path.dirname(os.path.realpath(__file__))
 
 
 def parse_ros2bag(bag, output, camera_num, blur):
-    print(bag)
-
     image_topic_names = []
     pointcloud_topic_names = []
     misc_topic_names = []
 
     topic_types = {
-            'std_msgs/msg/Int32': misc_topic_names,
-
             'sensor_msgs/msg/Image': image_topic_names,
             'sensor_msgs/msg/PointCloud2': pointcloud_topic_names,
-            'sensor_msgs/msg/NatSatFix': misc_topic_names,
+            'sensor_msgs/msg/NavSatFix': misc_topic_names,
             'sensor_msgs/msg/TimeReference': misc_topic_names,
-
             'geometry_msgs/msg/TwistStamped': misc_topic_names,
-
             'tf2_msgs/msg/TFMessage': misc_topic_names,
+            'rcl_interfaces/msg/Log': misc_topic_names,
+            'std_msgs/msg/Int32': misc_topic_names,
             }
-    # TODO some of these are not just misc
 
     # get topics
     info = rosbag2_py.Info()
@@ -32,12 +30,12 @@ def parse_ros2bag(bag, output, camera_num, blur):
 
     # separate topic types we care about into lists
     for t in metadata.topics_with_message_count:
-        print(t.topic_metadata.name, '\t', t.topic_metadata.type, '\n')
         topic_names = topic_types.get(t.topic_metadata.type)
         if topic_names is not None:
             topic_names.append(t.topic_metadata.name)
 
     # export pointclouds
+    pointcloud_export_processes = []
     for t in pointcloud_topic_names:
         subprocess.Popen([
             'ros2', 'bag', 'export',
@@ -64,25 +62,64 @@ def parse_ros2bag(bag, output, camera_num, blur):
         '-o', output + '/misc_topics'
         ])
 
-    # wait for image exports to finish
-    for p in image_export_processes:
-        p.wait()
-
     # wait for misc extract to finish
     misc_extract_process.wait()
 
-    # TODO sync
-    # TODO export synced images
+    conversion_process = subprocess.Popen(['ros2bag-convert', output + '/misc_topics/misc_topics_0.db3'])
 
-    # TODO blur images (synced and not synced)
+    # wait for conversion to csv to finish
+    conversion_process.wait()
+
+    subprocess.Popen([
+        'python3', scriptpath + '/ros2-csv-kml_converter/csv-to-kml.py',
+        'misc_topics'],
+        cwd=output)
+
+    sync_process = subprocess.Popen([
+        'ros2', 'bag', 'sync',
+        bag,
+        '-t', *image_topic_names,
+        '-o', output + '/synced_topics',
+        ])
+
+    # wait for syncronization to finish
+    sync_process.wait()
+
+    # export synced images (with the same topic names as before)
+    synced_image_export_processes = []
+    for t in image_topic_names:
+        synced_image_export_processes.append(subprocess.Popen([
+            'ros2', 'bag', 'export',
+            '--in', output + '/synced_topics/synced_topics_0.db3',
+            '-t', t, 'image',
+            '--dir', output + '/synced_topics' + t,
+            ]))
+
+    if blur:
+        # wait for image exports to finish and start blurring
+        for p, t in zip(image_export_processes, image_topic_names):
+            p.wait()
+            subprocess.run([
+                'python3', 'licenseplate_test.py',
+                '-i', output + t,
+                '-o', output + '/blurred_images' + t,
+                ],
+                cwd=scriptpath + '/person_and_licenceplate_blurring')
+    #
+    #     # wait for synced image exports to finish
+    #     for p in synced_image_export_processes:
+    #         p.wait()
+    #         # TODO go through synced, replace with blurred
 
     # TODO create video
 
-    # TODO extract misc into new bag
-    # TODO export to csv
-    # TODO convert to kml
+    # wait for pointcloud exports to finish
+    for p in pointcloud_export_processes:
+        p.wait()
 
     # TODO file structure
+
+    # TODO separate pipelines to separate subprocesses
 
 
 if __name__ == '__main__':
@@ -100,6 +137,12 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--blur',
                         action='store_true',
                         help='Blur faces and license plates')
+    # TODO verbose/silent
+    # TODO keep intermediary files
+    # TODO checks
 
     args = parser.parse_args()
-    parse_ros2bag(args.input, args.output_dir, args.camera_num, args.blur)
+    parse_ros2bag(args.input,
+                  os.path.realpath(args.output_dir),
+                  args.camera_num,
+                  args.blur)
