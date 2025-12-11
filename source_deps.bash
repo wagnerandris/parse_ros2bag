1c1e969221dcc9f99d296d9dfd089364e93c6e45
kovbando <kovbando@gmail.com>
2025-12-11 22:02:19 +0100

    updated source_deps.bash, to source the system-wide ros2 underlay if it is not sourced yet; Updated config, and added a few comments for handling preview generation and image compression

--- source_deps.bash
+++ source_deps.bash
@@ -1,3 +1,4 @@
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source /opt/ros/humble/setup.bash # Source the system-wide RO2 just to be sure it is sourced
 source "${SCRIPT_PATH}/install/setup.bash" # ROS2 packages
 source "${SCRIPT_PATH}/venv/bin/activate" # python venv
